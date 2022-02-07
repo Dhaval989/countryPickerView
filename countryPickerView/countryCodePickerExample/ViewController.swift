af3eb1d93cf164275ee53f4b20a40108f73f55e6
Dhaval989 <90313974+Dhaval989@users.noreply.github.com>
2022-02-07 18:01:42 +0530

    file add through drag and drop
    we create a pod file

---
+++ countryPickerView/countryCodePickerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  countryCodePickerExample
+//
+//  Created by Admin on 07/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ countryPickerView/countryPickerView/countryPickerView.h
@@ -0,0 +1,18 @@
+//
+//  countryPickerView.h
+//  countryPickerView
+//
+//  Created by Admin on 07/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for countryPickerView.
+FOUNDATION_EXPORT double countryPickerViewVersionNumber;
+
+//! Project version string for countryPickerView.
+FOUNDATION_EXPORT const unsigned char countryPickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <countryPickerView/PublicHeader.h>
+
+
